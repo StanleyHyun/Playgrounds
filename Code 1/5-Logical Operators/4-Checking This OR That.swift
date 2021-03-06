d99f2092c4beed68266a437e75ebe6511f9ef1b4
StanleyHyun <76261820+StanleyHyun@users.noreply.github.com>
2021-03-06 04:06:17 -0500

    Update 4-Checking This OR That.swift

--- Code 1/5-Logical Operators/4-Checking This OR That.swift
+++ Code 1/5-Logical Operators/4-Checking This OR That.swift
@@ -1,1 +1,13 @@
 //
+
+    for i in 1 ... 12 {
+        if isBlocked || isBlockedLeft{
+            turnRight()
+        }
+        moveForward()
+        if isOnGem {
+            collectGem()
+        }
+        
+    }
+
