ef98f01ca4235ffa8abb513e01bbcb83f7a474e2
StanleyHyun <76261820+StanleyHyun@users.noreply.github.com>
2021-03-06 04:05:56 -0500

    Update 5-Logical Labyrinth.swift

--- Code 1/5-Logical Operators/5-Logical Labyrinth.swift
+++ Code 1/5-Logical Operators/5-Logical Labyrinth.swift
@@ -1,1 +1,40 @@
-//
+doTheStairs()
+moveForwardTwice()
+toggleSwitch()
+turnLeft()
+moveForward()
+doTheStairs()
+moveForward()
+toggleSwitch()
+turnLeft()
+moveForward()
+collectGem()
+doTheStairs()
+
+
+
+
+
+
+func ToggleAndCollect() {
+    collectGem()
+    toggleSwitch()
+}
+func doTheStairs() {
+    moveForward()
+    ToggleAndCollect()
+    turnRight()
+    moveForwardTwice()
+    collectGem()
+    TurnAround()
+    moveForwardTwice()
+    turnRight()
+}
+func TurnAround() {
+    turnRight()
+    turnRight()
+}
+func moveForwardTwice() {
+    moveForward()
+    moveForward()
+}
