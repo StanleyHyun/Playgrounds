14060b9dce883207f98ce753105ff1670114c948
StanleyHyun <76261820+StanleyHyun@users.noreply.github.com>
2021-03-06 04:06:36 -0500

    Update 3-Checking This AND That.swift

--- Code 1/5-Logical Operators/3-Checking This AND That.swift
+++ Code 1/5-Logical Operators/3-Checking This AND That.swift
@@ -1,1 +1,34 @@
 //
+//
+func checkTile() {
+    if isOnGem && isBlockedLeft {
+        collectGem()
+        doTheStairs()
+    }else{
+        collectGem()
+    }
+    
+    
+    }
+        
+    
+        
+for i in 1 ... 7 {
+    moveForward()
+    
+    checkTile()
+}
+
+    
+    
+func doTheStairs() {
+    turnRight()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    turnRight()
+}
