c1be3da0efe2d5c498ea41f7527003c4f7defe4a
stanley <76261820+StanleyHyun@users.noreply.github.com>
2021-03-06 08:25:04 -0500

    I AM DONE
    COMPLETE

--- Code 1/6-While Loops/4-Four by Four.swift
+++ Code 1/6-While Loops/4-Four by Four.swift
@@ -1,1 +1,20 @@
 //
+func DoTheFirstPart() {
+    for i in 1 ... 3 {
+        moveForward()
+    }
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }
+    turnRight()
+}
+
+for i in 1 ... 3 {
+    DoTheFirstPart()
+}
+for i in 1 ... 3 {
+    moveForward()
+}
+if isOnClosedSwitch {
+    toggleSwitch()
+}
