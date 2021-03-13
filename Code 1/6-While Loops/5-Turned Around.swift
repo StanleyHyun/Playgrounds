54e64e333cb7df47555d7cdc8dca0a8d731785a5
stanley <76261820+StanleyHyun@users.noreply.github.com>
2021-03-13 08:42:49 -0500

    Update 5-Turned Around.swift
    Not Fineshed

--- Code 1/6-While Loops/5-Turned Around.swift
+++ Code 1/6-While Loops/5-Turned Around.swift
@@ -1,1 +1,18 @@
 //
+moveForward()
+
+while isOnGem {
+    collectGem()
+    turnLeft()
+    if !isBlocked{
+        moveForward()
+        collectGem()
+    }else{
+        turnLeft()
+        moveForward()
+        turnRight()
+        moveForward()
+        turnLeft()
+    }
+    
+}
