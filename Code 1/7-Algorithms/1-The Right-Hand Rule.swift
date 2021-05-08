4f79ce2988000299c433f7bac94b9bff3f5dd2f8
‍Blue <76261820+StanleyHyun@users.noreply.github.com>
2021-05-08 08:28:50 -0400

    Update 1-The Right-Hand Rule.swift

--- Code 1/7-Algorithms/1-The Right-Hand Rule.swift
+++ Code 1/7-Algorithms/1-The Right-Hand Rule.swift
@@ -1,1 +1,24 @@
 //
+func turnAround() {
+    turnLeft()
+    turnLeft()
+}
+
+func navigateAroundWall() {
+    if isBlockedRight {
+        moveForward()
+    }  else {
+        turnRight()
+        moveForward()
+    }
+}
+
+while !isOnClosedSwitch {
+    navigateAroundWall()
+    if isOnGem {
+        collectGem()
+        turnAround()
+    }
+}
+toggleSwitch()
+
