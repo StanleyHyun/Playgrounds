834702d056c10cb18c15e366753fe22cdc523dbe
‍Blue <76261820+StanleyHyun@users.noreply.github.com>
2021-05-15 08:10:25 -0400

    Update 2-Adjusting Your Algorithm.swift

--- Code 1/7-Algorithms/2-Adjusting Your Algorithm.swift
+++ Code 1/7-Algorithms/2-Adjusting Your Algorithm.swift
@@ -1,1 +1,22 @@
 //
+func navigateAroundWall() {
+    if isBlockedRight {
+        if isBlocked {
+            turnLeft()
+        }
+        moveForward()
+    }  else {
+        turnRight()
+        moveForward()
+    }
+}
+
+while !isOnClosedSwitch {
+    navigateAroundWall()
+    if isOnGem {
+        collectGem()
+        turnLeft()
+        turnLeft()
+    }
+}
+toggleSwitch()
