3641305bcf7c956f5b6f35baf9fb8841926a613a
‍Blue <76261820+StanleyHyun@users.noreply.github.com>
2021-05-22 08:31:32 -0400

    Update 4-Which Way to Turn.swift

--- Code 1/7-Algorithms/4-Which Way to Turn.swift
+++ Code 1/7-Algorithms/4-Which Way to Turn.swift
@@ -1,1 +1,35 @@
 //
+func part1() {
+    while !isOnClosedSwitch && !isOnGem {
+        moveForward()
+    }
+    toggleSwitch()
+    turnRight()
+}
+
+func part2() {
+    while !isBlocked && !isOnGem {
+        moveForward()
+    }
+        turnLeft()
+        toggleSwitch()
+    }
+    
+
+/*
+ while you are not at the gem continue the algorithm (continue parts 1 and 2)
+ part1
+ if you are not blocked in the front and on a switch toggle the switch and make a right turn.
+ part2
+ if you are blocked in the front and on a switch then toggle the switch and make a left turn.
+ */
+
+
+
+while !isOnGem {
+    part1()
+    part2()
+    part2()
+}
+collectGem()
+
