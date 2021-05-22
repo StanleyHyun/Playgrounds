1dcd53f704e03c0f0c83bdb0230d5bf8c68a0621
‍Blue <76261820+StanleyHyun@users.noreply.github.com>
2021-05-22 08:50:32 -0400

    Update 5-Roll Right Roll Left.swift

--- Code 1/7-Algorithms/5-Roll Right Roll Left.swift
+++ Code 1/7-Algorithms/5-Roll Right Roll Left.swift
@@ -1,1 +1,26 @@
 //
+/*
+ I need to make a function for the gems and the switches
+ 
+ For the gems
+ i need to make a function called Collect gems and i will moveforward
+ collectgem and turn right and move forward collect gem.
+ 
+ For the switches
+ i need to toggle switch and turn right 
+ and then toggle another switch.
+ */
+
+
+func Switches() {
+    toggleSwitch()
+    turnRight()
+    toggleSwitch()
+}
+
+func collectGems() {
+    moveForward()
+    collectGem()
+    turnLeft()
+    collectGem()
+}
