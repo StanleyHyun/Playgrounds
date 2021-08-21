d431903403cf472b893c4c474fcf3332c729b52f
Stanley Hyun <76261820+StanleyHyun@users.noreply.github.com>
2021-08-21 10:04:26 -0400

    Update 5-Logical Labyrinth.swift

--- Code 1/5-Logical Operators/5-Logical Labyrinth.swift
+++ Code 1/5-Logical Operators/5-Logical Labyrinth.swift
@@ -1,40 +1,77 @@
-doTheStairs()
-moveForwardTwice()
-toggleSwitch()
-turnLeft()
-moveForward()
-doTheStairs()
-moveForward()
-toggleSwitch()
-turnLeft()
-moveForward()
-collectGem()
-doTheStairs()
+/*
+ When There is a toggle switch and a gem you need to turn right
+ When there is a closed Swited you need to turn left
+ 
+ if isOnGem && isOnClosedSwitch{
+ solveThePattern()
+ }
+ continuemoving if im on an openswitch 
+ if im on closed switch then turn left
+ 
+ 
+ */
 
 
+func doTheL() {
+    moveForward()
+    if isOnGem && isOnClosedSwitch {
+        turnRight()
+        solveThePattern()
+        collectAndToggle()
+        
+    }
+    
+}
 
-
-
-
-func ToggleAndCollect() {
+func collectAndToggle() {
     collectGem()
     toggleSwitch()
 }
-func doTheStairs() {
-    moveForward()
-    ToggleAndCollect()
-    turnRight()
+
+
+func solveThePattern() {
     moveForwardTwice()
     collectGem()
-    TurnAround()
+    turnAround()
     moveForwardTwice()
     turnRight()
+    
 }
-func TurnAround() {
-    turnRight()
-    turnRight()
+
+
+
+
+func turnAround() {
+    turnLeft()
+    turnLeft();
 }
+
+
 func moveForwardTwice() {
-    moveForward()
-    moveForward()
+    for i in 1 ... 2 {
+        moveForward()
+    }
+    
 }
+
+
+
+    doTheL()
+    moveForwardTwice()
+    if isOnClosedSwitch{
+        toggleSwitch()
+        turnLeft()
+        moveForward()
+        
+    }
+doTheL()
+moveForward()
+toggleSwitch()
+turnLeft()
+doTheL()
+collectGem()
+doTheL()
+
+
+
+
