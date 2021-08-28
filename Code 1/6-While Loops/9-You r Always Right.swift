11f0670fe5b6ac592a2799765e8990e928290385
Stanley Hyun <76261820+StanleyHyun@users.noreply.github.com>
2021-08-28 09:52:28 -0400

    Update 9-You r Always Right.swift

--- Code 1/6-While Loops/9-You r Always Right.swift
+++ Code 1/6-While Loops/9-You r Always Right.swift
@@ -1,32 +1,42 @@
 //
-while !isBlocked {
-    for i in 1 ... 3 {
-        checkTheMoveForward()
-        if isBlocked {
-            turnRight()
+/*
+ when you are blocked you need to turn right
+ And you need to check each square to see if it is on an open switch or a closed
+ switch
+ 
+ you need to keep on doing this until you are on a gem
+ 
+ while !isongem{
+ solve puzzle
+ }
+ 
+ 
+ 
+ 
+ */
+
+func solveTheFirstRow() {
+    while !isBlocked {
+        if isOnClosedSwitch {
+            toggleSwitch()
         }
-        
-    }
-        
-    }
-    
-    
-
-
-func checkTheMoveForward() {
-    checkTheSquare()
-    moveForward()
-    
-}
-
-func checkTheSquare() {
-    if isOnClosedSwitch {
-        toggleSwitch()
-    }else if isOnOpenSwitch{
         moveForward()
     }
     
-        
-    }
+}
+
+
+
+
+while !isOnGem {
+    solveTheFirstRow()
+    turnRight()
     
+    
+}
+collectGem()
+
+
+
+
 
